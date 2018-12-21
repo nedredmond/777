1b00891a0f631dc2da6cbe0df7675c2320a50f71
nicholassatinover <nsatinover@gmail.com>
2018-12-20 22:11:17 -0500

    Data Loader Bill Item

--- src/main/java/io/zcw/zipmint/DataLoaderBillItem.java
+++ src/main/java/io/zcw/zipmint/DataLoaderBillItem.java
@@ -4,28 +4,29 @@ import io.zcw.zipmint.domain.BillItem;
 import io.zcw.zipmint.repository.BillItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import java.time.LocalDate;
+import java.util.LinkedHashSet;
 
 @Component
 public class DataLoaderBillItem {
 
     private BillItemRepository billItemRepository;
-    // private LocalDate localDate
+    private LinkedHashSet<BillItem> billItemSet = new LinkedHashSet<>();
 
     @Autowired
     public DataLoaderBillItem(BillItemRepository billItemRepository){
         this.billItemRepository = billItemRepository;
-        LoadBills();
+        LoadBillItems();
     }
 
-    private void LoadBills(){
+    private void LoadBillItems(){
         BillItem billItem = new BillItem();
         billItem.setCompanyName("Utility Company");
         billItem.setDueDate(LocalDate.of(2019, 1, 30));
         billItem.setPaymentAmount(100L);
         billItem.setAutoPay(Boolean.TRUE);
         billItemRepository.save(billItem);
+        billItemSet.add(billItem);
 
         BillItem billItem2 = new BillItem();
         billItem2.setCompanyName("Water Company");
@@ -33,5 +34,10 @@ public class DataLoaderBillItem {
         billItem2.setPaymentAmount(50L);
         billItem2.setAutoPay(Boolean.FALSE);
         billItemRepository.save(billItem2);
+        billItemSet.add(billItem2);
+    }
+
+    public LinkedHashSet<BillItem> getBillItemSet() {
+        return billItemSet;
     }
 }
