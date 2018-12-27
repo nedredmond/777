72303c5d9be9bd9a0b9e376e6dbde54f9b21ef2f
nicholassatinover <nsatinover@gmail.com>
2018-12-27 00:26:11 -0500

    pulling down transactions

--- src/main/java/io/zcw/zipmint/DataLoaderBillItem.java
+++ src/main/java/io/zcw/zipmint/DataLoaderBillItem.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import java.time.LocalDate;
 import java.util.LinkedHashSet;
-//test
 
 @Component
 public class DataLoaderBillItem {
@@ -24,16 +23,16 @@ public class DataLoaderBillItem {
     @PostConstruct
     private void LoadBillItems(){
         BillItem billItem = new BillItem();
-        billItem.setCompanyName("Utility Company");
-        billItem.setDueDate(LocalDate.of(2019, 1, 30));
+        billItem.setCompanyName("Utility Company Nov");
+        billItem.setDueDate(LocalDate.of(2018, 11, 15));
         billItem.setPaymentAmount(100L);
         billItem.setAutoPay(Boolean.TRUE);
         billItemRepository.save(billItem);
         billItemSet.add(billItem);
 
         BillItem billItem2 = new BillItem();
-        billItem2.setCompanyName("Water Company");
-        billItem2.setDueDate(LocalDate.of(2019, 1, 15));
+        billItem2.setCompanyName("Utility Company Dec");
+        billItem2.setDueDate(LocalDate.of(2018, 12, 15));
         billItem2.setPaymentAmount(50L);
         billItem2.setAutoPay(Boolean.FALSE);
         billItemRepository.save(billItem2);
